99e5e8572e28e480bdd95c8e8beeecefe6b9989f
Alice <Grayhild@mail.ru>
2025-03-08 01:08:10 +0700

    изменения

--- config/celery.py
+++ config/celery.py
@@ -6,7 +6,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Создание экземпляра объекта Celery
-app = Celery('config')
+app = Celery('config', broker="redis://redis:6379/2", include=[
+        "school.tasks",
+    ],)
 
 # Загрузка настроек из файла Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
